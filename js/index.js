46b614e360f9ff4592490a58c6cb896680681380
mariamartinezcano <mariamartinezcano@knights.ucf.edu>
2021-11-19 11:41:12 -0500

    adding offset

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 $(".aboutMe").click(function() {
     $('html, body').animate({
-        scrollTop: $(".about-me").offset().top
+        scrollTop: $(".about-me").offset().top-110
     }, 1000);
 });
 
 $(".myProjects").click(function() {
     $('html, body').animate({
-        scrollTop: $(".my-work").offset().top
+        scrollTop: $(".my-work").offset().top-50
     }, 1000);
 });
 

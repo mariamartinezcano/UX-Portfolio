5d9baa2851ad1d7c4fac3d48d2aaef2baab0d0dd
mariamartinezcano <mariamartinezcano@knights.ucf.edu>
2021-11-19 11:02:47 -0500

    speed change

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 $(".aboutMe").click(function() {
     $('html, body').animate({
         scrollTop: $(".about-me").offset().top
-    }, 200);
+    }, 500);
 });
 
 $(".myProjects").click(function() {
     $('html, body').animate({
         scrollTop: $(".my-work").offset().top
-    }, 200);
+    }, 500);
 });
 
 $(".contactMe").click(function() {
     $('html, body').animate({
         scrollTop: $("footer").offset().top
-    }, 200);
+    }, 500);
 });

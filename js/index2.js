80f0ef8942652efc530949b6e1238db8548e265b
hooman-faily <82850123+hooman-faily@users.noreply.github.com>
2021-11-19 11:53:25 -0500

    new page added

--- js/index2.js
+++ js/index2.js
@@ -1,12 +1,12 @@
 $(".aboutMe").click(function() {
     $('html, body').animate({
-        scrollTop: $(".about-me").offset().top-120
+        scrollTop: $("#about-me").offset().top-120
     }, 1000);
 });
 
 $(".myProjects").click(function() {
     $('html, body').animate({
-        scrollTop: $(".my-work").offset().top-120
+        scrollTop: $("#my-work").offset().top-120
     }, 1000);
 });
 
